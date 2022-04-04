d9371fb8e1e722bebba82ee0cff194b33bf7fb99
unknown <jemmanuel@student.ciputra.ac.id>
2022-04-04 16:21:06 +0700

    Init

---
+++ Assets/SnakeMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.Translate(0, 0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.Translate(0, 0, -1);
+        }
+        if (transform.position.x < 0)
+        {
+            transform.Translate(1, 0, 0);
+        }
+        if (transform.position.x > 3)
+        {
+            transform.Translate(-1, 0, 0);
+        }
+        if (transform.position.y < 0)
+        {
+            transform.Translate(0, 1, 0);
+        }
+        if (transform.position.y > 3)
+        {
+            transform.Translate(0, -1, 0);
+        }
+        if (transform.position.z < 0)
+        {
+            transform.Translate(0, 0, 1);
+        }
+        if (transform.position.z > 3)
+        {
+            transform.Translate(0, 0, -1);
+        }
+    }
+}
