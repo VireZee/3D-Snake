c498f80d7c146d98b99d52682d0be2323484240f
unknown <jemmanuel903@gmail.com>
2022-05-30 16:27:27 +0700

    Body

--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    public List<GameObject> body;
+    public List<GameObject> listBody;
     public GameObject bodyPrefab;
+    private Vector3 positionBefore;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +17,28 @@ public class Snake : MonoBehaviour
     {
         
     }
-    public void CreateSnake()
+    private void CreateSnake()
     {
         AddBody();
     }
-    public void AddBody()
+    private void AddBody()
     {
         var newBody = Instantiate(bodyPrefab);
-        body.Add(newBody);
+        listBody.Add(newBody);
+    }
+    public void savePosition()
+    {
+        positionBefore = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+    public void updatePosition()
+    {
+        var lastPosition = new Vector3(positionBefore.x, positionBefore.y, positionBefore.z);
+        foreach (var body in listBody)
+        {
+            var bodyPosition = new Vector3(body.transform.position.x, body.transform.position.y, body.transform.position.z);
+            body.transform.position = new Vector3(lastPosition.x, lastPosition.y, lastPosition.z);
+            lastPosition = new Vector3(bodyPosition.x, bodyPosition.y, bodyPosition.z);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
--- Assets/SnakeMovement.cs
+++ Assets/SnakeMovement.cs
@@ -4,61 +4,91 @@ using UnityEngine;
 
 public class SnakeMovement : MonoBehaviour
 {
+    private Snake snake;
+    private string directionBefore;
     // Start is called before the first frame update
     void Start()
     {
-        
+        snake = GetComponent<Snake>();
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
+            snake.savePosition();
             transform.Translate(0, 1, 0);
+            snake.updatePosition();
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
+            snake.savePosition();
             transform.Translate(-1, 0, 0);
+            snake.updatePosition();
         }
         if (Input.GetKeyDown(KeyCode.K))
         {
+            snake.savePosition();
             transform.Translate(0, -1, 0);
+            snake.updatePosition();
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
+            snake.savePosition();
             transform.Translate(1, 0, 0);
+            snake.updatePosition();
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
+            snake.savePosition();
             transform.Translate(0, 0, 1);
+            snake.updatePosition();
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
+            snake.savePosition();
             transform.Translate(0, 0, -1);
+            snake.updatePosition();
         }
         if (transform.position.x < 0)
         {
+            snake.savePosition();
             transform.Translate(1, 0, 0);
+            snake.updatePosition();
         }
         if (transform.position.x > 4)
         {
+            snake.savePosition();
             transform.Translate(-1, 0, 0);
+            snake.updatePosition();
         }
         if (transform.position.y < 0)
         {
+            snake.savePosition();
             transform.Translate(0, 1, 0);
+            snake.updatePosition();
         }
         if (transform.position.y > 4)
         {
+            snake.savePosition();
             transform.Translate(0, -1, 0);
+            snake.updatePosition();
         }
         if (transform.position.z < 0)
         {
+            snake.savePosition();
             transform.Translate(0, 0, 1);
+            snake.updatePosition();
         }
         if (transform.position.z > 4)
         {
+            snake.savePosition();
             transform.Translate(0, 0, -1);
+            snake.updatePosition();
         }
+    }
+    private void saveDirection()
+    {
+        directionBefore = 
     }
 }
